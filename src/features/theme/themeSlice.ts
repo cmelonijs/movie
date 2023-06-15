9bff9cce5dd4b67349b37dde7001e6d5396e355b
cmeloni <carlo.meloni@abinsula.com>
2023-06-15 17:13:25 +0200

    feat: adding dispatch and selector hooks and creating theme slice

---
+++ src/features/theme/themeSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type ThemeState = boolean;
+
+const initialState: ThemeState = false;
+
+const themeFromLocalStorage = !!localStorage.getItem("movie-theme");
+
+export const themeSlice = createSlice({
+    name: "theme",
+    initialState,
+    reducers: {
+        toggleTheme: (state) => {
+            if (state) {
+                localStorage.removeItem("movie-theme");
+            } else {
+                localStorage.setItem("movie-theme", "_");
+            }
+            return !state;
+        }
+    }
+});
+
+export const { toggleTheme } = themeSlice.actions;
+
+export default themeSlice.reducer;
