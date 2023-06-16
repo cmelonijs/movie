7a2eaeb2cb5e9a7d271953b2e734be14a2be0162
cmeloni <carlo.meloni@abinsula.com>
2023-06-16 10:16:19 +0200

    feat: adding a movies slice for fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import Header from "./components/Header/Header";
-import { useAppSelector } from "./hooks/storeHook";
+import { getMovies } from "./features/movies/moviesSlice";
+import { useAppDispatch, useAppSelector } from "./hooks/storeHook";
+import { useEffect } from "react";
 
 function App() {
   const { darkTheme } = useAppSelector((state) => state);
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getMovies())
+  }, [])
+    
   return (
     <h2 className={darkTheme ? "dark" : ""}>
       <div className="dark:bg-black dark:text-white min-h-screen px-4 lg:px-12 pb-20">
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./features/theme/themeSlice";
+import moviesReducer from "./features/movies/moviesSlice";
 
 export const store = configureStore({
   reducer: {
     darkTheme: themeReducer,
+    movies: moviesReducer,
   },
 });
 
