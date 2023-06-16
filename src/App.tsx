e3550486b8685faeee69b6e03453826c26ff1b2a
cmeloni <carlo.meloni@abinsula.com>
2023-06-16 11:28:42 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,31 @@ import { useEffect } from "react";
 function App() {
   const { darkTheme, movies } = useAppSelector((state) => state);
 
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getMovies())
-  }, [dispatch])
-    
+    dispatch(getMovies());
+  }, [dispatch]);
 
-  console.log('movies', movies)
+  console.log("movies", movies);
   return (
     <h2 className={darkTheme ? "dark" : ""}>
       <div className="dark:bg-gray-800 dark:text-white min-h-screen px-4 lg:px-12 pb-20">
         <Header />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-10">
-          {movies.movies && movies.movies.results.map((movie: any) => {
-            return (
-              <MovieCard
-                key={movie.id}
-                poster_path={"https://image.tmdb.org/t/p/original" + movie.poster_path}
-                overview={movie.overview}
-                title={movie.title}
-              />
-            );
-          })}
+          {movies.movies &&
+            movies.movies.results.map((movie: any) => {
+              return (
+                <MovieCard
+                  key={movie.id}
+                  poster_path={
+                    "https://image.tmdb.org/t/p/original" + movie.poster_path
+                  }
+                  overview={movie.overview}
+                  title={movie.title}
+                />
+              );
+            })}
         </div>
       </div>
     </h2>
--- src/features/movies/moviesSlice.ts
+++ src/features/movies/moviesSlice.ts
@@ -1,48 +1,57 @@
-import {createSlice, PayloadAction, createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 
 // action
-export const getMovies = createAsyncThunk("movies/getMovies", async (data, thunkApi) => {
+export const getMovies = createAsyncThunk(
+  "movies/getMovies",
+  async (data, thunkApi) => {
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIE_KEY}`)
+      const response = await fetch(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIE_KEY}`
+      );
 
-        return await response.json()
-    } catch(err: any) {
-        return thunkApi.rejectWithValue(err.message)
+      return await response.json();
+    } catch (err: any) {
+      return thunkApi.rejectWithValue(err.message);
     }
-})
+  }
+);
 
 interface MoviesState {
-    loading: boolean;
-    error: string | null;
-    movies: null | {results: any[]}
+  loading: boolean;
+  error: string | null;
+  movies: null | { results: any[] };
 }
 
 const initialState: MoviesState = {
-    loading: false,
-    error: null,
-    movies: null
-}
+  loading: false,
+  error: null,
+  movies: null,
+};
 
 //Slice
 const moviesSlice = createSlice({
-    name: "movies",
-    initialState,
-    reducers: {},
-    extraReducers: {
-        [getMovies.pending.type]: (state) => {
-            state.loading = true
-        },
-        [getMovies.fulfilled.type]: (state, action: PayloadAction<{results: any[]}>) => {
-            state.loading = false
-            state.movies = action.payload
-        },
-        [getMovies.rejected.type]: (state, action: PayloadAction<string | null>) => {
-            state.loading = false
-            state.error = action.payload
-        } 
-    }
-})
+  name: "movies",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getMovies.pending.type]: (state) => {
+      state.loading = true;
+    },
+    [getMovies.fulfilled.type]: (
+      state,
+      action: PayloadAction<{ results: any[] }>
+    ) => {
+      state.loading = false;
+      state.movies = action.payload;
+    },
+    [getMovies.rejected.type]: (
+      state,
+      action: PayloadAction<string | null>
+    ) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
 
 export default moviesSlice.reducer;
-
-
